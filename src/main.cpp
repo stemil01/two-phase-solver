47e47ab19a9bbb697a38c1f8887ef940122db013
stemil01 <stefan180501@gmail.com>
2020-03-30 21:20:19 +0200

    treba da su moving tables u potpunosti gotove i funkcijonalne

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 #include "definitions.hpp"
 #include "moves.hpp"
 using namespace std;
@@ -10,32 +9,4 @@ int main()
     // input(&cube);
 
     generate_moving_tables();
-    int x;
-    Edges temp[4];
-    cin >> x;
-    UDslice_edge_permutation_to_cubie(x, temp);
-    for (int i = 0; i < 4; i++)
-        cout << temp[i] << " ";
-    cout << '\n';
-
-    for (int move = 0; move < 18; move += 3)
-    {
-        if (move < 12)
-        {
-            cout << move  + 1 << ": ";
-            UDslice_edge_permutation_to_cubie(move_UDslice_edge_permutation[x][move + 1], temp);
-            for (int i = 0; i < 4; i++)
-                cout << temp[i] << " ";
-            cout << '\n';
-        }
-        else
-            for (int j = 0; j < 3; j++)
-            {
-                cout << move + j << ": ";
-                UDslice_edge_permutation_to_cubie(move_UDslice_edge_permutation[x][move + j], temp);
-                for (int i = 0; i < 4; i++)
-                    cout << temp[i] << " ";
-                cout << '\n';
-            }
-    }
 }
