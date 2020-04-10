4226c0f80346a6cc772a8ade0d6dad937b8480f6
stemil01 <stefan180501@gmail.com>
2020-04-10 19:37:36 +0200

    nemam pojma sta radim

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # two-phase-solver
-Pokusaj da se implementira ovaj algoritam.
+Implementacija two phase algoritma Herberta Kociembe za resavanje Rubikove kocke.
+Algoritam nije optimalan po broju poteza, ali zato daje resenja duzeine priblizno optimalnom. 
 
-U trenutku kada je zapoceto ovo ja ne znam da li sam u mogucnosti da ovako nesto uradim i zato samo pokusavam tako sto cu da izbacim simetrije.
-Nadam se da cu uskoro da saznam na cemu sam.
+## rezultati
+Jos uvek nisam testirao program kako dolikuje ali za sada deluje da gotovo odmah daje resenja duzine do 26 poteza, prosek mi deluje da je oko 22-23 poteza.
+Resenja prve faze su ocekivano brza jer nema gotovo nikakvog dodatnog racuna, dok se resenja druge faze takodje dosta brzo dobijaju, na moje iznenadjenje.
+
+## pokretanje
+Program je kompletno pisan u C++ i za kompajlovanje je koriscen GCC i testiran je na GNU/Linux sistemu, na kome se ocekuje da radi. Nije testirano ali bi verovatno isto radilo na bilo kom Unix sistemu. Da bi se pokrenuo moze se pozvati komandom 
+```shell
+$ make
+```
--- src/main.cpp
+++ src/main.cpp
@@ -8,79 +8,84 @@ using namespace std;
 
 int main()
 {
+    srand(time(NULL));
+
     generate_moving_tables();
     generate_phase1_pruning_table();
     generate_phase2_pruning_tables();
 
     // testiranje pretrage
-    int corner_orientation = 0, edge_orientation = 0, UDslice_edge_position = 0;
-    int corner_permutation = 0, UD_edge_permutation = 0, UDslice_edge_permutation = 0;
-    Edges cubie_edge_permutation[12];
-    Edges moved[12];
-
-    for (int i = 0; i < 12; i++)
-        cubie_edge_permutation[i] = (Edges)i;
-
-    cout << "Potezi za kocku: ";
-    for (int i = 0; i < 20; i++)
+    while (true)
     {
-        int move = rand() % 18;
-        print_move(move);
+        int corner_orientation = 0, edge_orientation = 0, UDslice_edge_position = 0;
+        int corner_permutation = 0, UD_edge_permutation = 0, UDslice_edge_permutation = 0;
+        Edges cubie_edge_permutation[12];
+        Edges moved[12];
 
-        corner_orientation = move_corner_orientation[corner_orientation][move];
-        edge_orientation = move_edge_orientation[edge_orientation][move];
-        UDslice_edge_position = move_UDslice_edge_position[UDslice_edge_position][move];
+        for (int i = 0; i < 12; i++)
+            cubie_edge_permutation[i] = (Edges)i;
 
-        corner_permutation = move_corner_permutation[corner_permutation][move];
+        cout << "Potezi za kocku: ";
+        for (int i = 0; i < 100; i++)
+        {
+            int move = rand() % 18;
+            print_move(move);
+
+            corner_orientation = move_corner_orientation[corner_orientation][move];
+            edge_orientation = move_edge_orientation[edge_orientation][move];
+            UDslice_edge_position = move_UDslice_edge_position[UDslice_edge_position][move];
+
+            corner_permutation = move_corner_permutation[corner_permutation][move];
+            Corners temp[8];
+            corner_permutation_to_cubie(corner_permutation, temp);
+
+            int base_move = (move / 3) * 3;
+            int rem_move = move % 3;
+            for (int j = 0; j <= rem_move; j++)
+            {
+                for (int k = 0; k < 12; k++)
+                    moved[k] = cubie_edge_permutation[move_edge[base_move][k]];
+                for (int k = 0; k < 12; k++)
+                    cubie_edge_permutation[k] = moved[k];
+            }
+        }
+        cout << '\n';
+
+        vector<int> v;
+        test_phase1_search(corner_orientation, edge_orientation, UDslice_edge_position, &v);
+
+        cout << "Resenje za prvu fazu bi trebalo da bude: ";
+        for (int i = 0; i < v.size(); i++)
+        {
+            print_move(v[i]);
+
+            corner_permutation = move_corner_permutation[corner_permutation][v[i]];
+            int base_move = (v[i] / 3) * 3;
+            int rem_move = v[i] % 3;
+            for (int j = 0; j <= rem_move; j++)
+            {
+                for (int k = 0; k < 12; k++)
+                    moved[k] = cubie_edge_permutation[move_edge[base_move][k]];
+                for (int k = 0; k < 12; k++)
+                    cubie_edge_permutation[k] = moved[k];
+            }
+        }
+        cout << '\n';
+
         Corners temp[8];
         corner_permutation_to_cubie(corner_permutation, temp);
 
-        int base_move = (move / 3) * 3;
-        int rem_move = move % 3;
-        for (int j = 0; j <= rem_move; j++)
-        {
-            for (int k = 0; k < 12; k++)
-                moved[k] = cubie_edge_permutation[move_edge[base_move][k]];
-            for (int k = 0; k < 12; k++)
-                cubie_edge_permutation[k] = moved[k];
-        }
+        Edges cubie_UD_edge_permutation[8], cubie_UDslice_edge_permutation[4];
+        for (int i = 0; i < 8; i++)
+            cubie_UD_edge_permutation[i] = cubie_edge_permutation[i];
+        for (int i = 0; i < 4; i++)
+            cubie_UDslice_edge_permutation[i] = cubie_edge_permutation[i + 8];
+        cubie_to_UD_edge_permutation(cubie_UD_edge_permutation, &UD_edge_permutation);
+        cubie_to_UDslice_edge_permutation(cubie_UDslice_edge_permutation, &UDslice_edge_permutation);
+        
+        test_phase2_search(corner_permutation, UD_edge_permutation, UDslice_edge_permutation);
+
+        // CubieLevelCube cube;
+        // input(&cube);
     }
-    cout << '\n';
-
-    vector<int> v;
-    test_phase1_search(corner_orientation, edge_orientation, UDslice_edge_position, &v);
-
-    cout << "Resenje za prvu fazu bi trebalo da bude: ";
-    for (int i = 0; i < v.size(); i++)
-    {
-        print_move(v[i]);
-
-        corner_permutation = move_corner_permutation[corner_permutation][v[i]];
-        int base_move = (v[i] / 3) * 3;
-        int rem_move = v[i] % 3;
-        for (int j = 0; j <= rem_move; j++)
-        {
-            for (int k = 0; k < 12; k++)
-                moved[k] = cubie_edge_permutation[move_edge[base_move][k]];
-            for (int k = 0; k < 12; k++)
-                cubie_edge_permutation[k] = moved[k];
-        }
-    }
-    cout << '\n';
-
-    Corners temp[8];
-    corner_permutation_to_cubie(corner_permutation, temp);
-
-    Edges cubie_UD_edge_permutation[8], cubie_UDslice_edge_permutation[4];
-    for (int i = 0; i < 8; i++)
-        cubie_UD_edge_permutation[i] = cubie_edge_permutation[i];
-    for (int i = 0; i < 4; i++)
-        cubie_UDslice_edge_permutation[i] = cubie_edge_permutation[i + 8];
-    cubie_to_UD_edge_permutation(cubie_UD_edge_permutation, &UD_edge_permutation);
-    cubie_to_UDslice_edge_permutation(cubie_UDslice_edge_permutation, &UDslice_edge_permutation);
-    
-    test_phase2_search(corner_permutation, UD_edge_permutation, UDslice_edge_permutation);
-
-    // CubieLevelCube cube;
-    // input(&cube);
 }
